45d6e3840719ceff8d166c44e9ff9552b6784f96
João Paulo Dias França <j.pdf00@gmail.com>
2022-09-19 08:56:02 -0300

    Adds username to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "username" do
+    # assert
+    it { is_expected.to validate_presence_of :username }
+  end
+
+  context "email" do
+    # assert
+    it { is_expected.to validate_presence_of :email }
+  end
+
+  context "password" do
+    # assert
+    it { is_expected.to validate_presence_of :password }
+    it { is_expected.to validate_length_of(:password).is_at_least(8) }
+    it { is_expected.to validate_length_of(:password).is_at_most(128) }
+  end
 end
